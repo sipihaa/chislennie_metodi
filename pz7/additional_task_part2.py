9004028adb001df0b809f3e7710b7d3551dc8418
Владислав <vladm@MacBook-Pro-Vladislav.local>
2025-05-12 19:31:46 +0300

    Добавлена комплексная часть

--- pz7/additional_task_part2.py
+++ pz7/additional_task_part2.py
@@ -2,8 +2,10 @@ import numpy as np
 from scipy.special import factorial
 
 
-def iterative_method(A, t, iterations=400):
-    res = np.eye(2)
+def iterative_method(A, theta=np.pi / 4, iterations=100):
+    res = np.eye(2, dtype=complex)
+    i = 1j
+    t = i * theta
 
     for k in range(1, iterations):
         res += ((A * t)**k) / factorial(k)
@@ -12,7 +14,9 @@ def iterative_method(A, t, iterations=400):
 
 
 A = np.ones((2, 2)) / 2
-t_list = [1, 5, 10]
-for t in t_list:
-    print(f'При t = {t}, выражение exp(At) равно:')
-    print(iterative_method(A, t))
+theta = np.pi / 4
+
+print(f'При θ = {theta:.3f}, выражение exp(A * i * θ) равно:')
+result = iterative_method(A, theta)
+formatted_result = np.array([[f"{x.real:.3f}{x.imag:+.3f}i" for x in row] for row in result])
+print(formatted_result)
